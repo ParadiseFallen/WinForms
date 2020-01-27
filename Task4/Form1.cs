4e0e5b8314cd9d05f0ee68e4271ca7e60750b69e
ParadiseFallen <JokerChYoutube@mail.ru>
2020-01-27 11:09:10 +0200

    5

--- Task4/Form1.cs
+++ Task4/Form1.cs
@@ -24,7 +24,6 @@ namespace Task4
             Start.X = e.X;
             Start.Y = e.Y;
         }
-        private List<Label> labels = new List<Label>();
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
             var sizeX = e.X-Start.X;
@@ -41,39 +40,49 @@ namespace Task4
                 ,Height = Start.Y > e.Y ?  Start.Y - e.Y : e.Y - Start.Y
                 ,BackColor = Color.Aqua
             };
-            labels.Add(t);
 
             t.MouseDown += (s, a) => {
                 if (a.Button != MouseButtons.Right)
                     return;
                 int maxN=0;
+
                 List<Label> temp = new List<Label>();
-                labels.ForEach(i => { if(i.ClientRectangle.Contains(e.X, e.Y)) temp.Add(i); });
+                GetLabels().ForEach(i => { if(new Rectangle(i.Location.X, i.Location.Y, i.Width,i.Height).Contains(e.Location)) temp.Add(i); });
                 foreach (var item in temp)
                 {
                     if (int.Parse(item.Text) > maxN)
                         maxN = int.Parse(item.Text);
                 }
-                Label l = labels.Where(i=> i.Text== maxN.ToString()).ElementAt<Label>(0);
+                Label l = GetLabels().Where(i=> i.Text== maxN.ToString()).ElementAt<Label>(0);
                 this.Text = $"S:{l.Width*l.Height}|X:{l.Left}|Y:{l.Top}";
             };
             t.MouseDoubleClick += (s, a) => {
                 if (a.Button != MouseButtons.Left)
                     return;
-
                 int minN = number;
-                List<Label> temp = new List<Label>();
-                labels.ForEach(i => { if (i.ClientRectangle.Contains(e.X, e.Y)) temp.Add(i); });
+
+                var temp = new List<Label>();
+                GetLabels().ForEach(i => { if (new Rectangle(i.Location.X, i.Location.Y, i.Width, i.Height).Contains(e.X, e.Y)) temp.Add(i); });
                 foreach (var item in temp)
                 {
                     if (int.Parse(item.Text) < minN)
                         minN = int.Parse(item.Text);
                 }
-                Label l = labels.Where(i => i.Text == minN.ToString())?.ElementAt<Label>(0);
+                Label l = GetLabels().Where(i => i.Text == minN.ToString())?.ElementAt<Label>(0);
                 Controls.Remove(l);
-                labels.Remove(l);
             };
+
             Controls.Add(t);
+        }
+
+        private List<Label> GetLabels() {
+            List<Label> t = new List<Label>();
+            foreach (var item in Controls)
+            {
+                if (item is Label)
+                    t.Add(item as Label);
+            }
+            return t;
         }
     }
 }
